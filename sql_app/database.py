af16769686f0faff879f47db3fb942243f1fa759
seungwoonsong <andrew960811@naver.com>
2023-08-01 05:28:35 +0000

    FEAT: env file added

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://susong:susong@localhost/umbrella"
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
