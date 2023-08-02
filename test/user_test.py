1c482cc16eb4b78ce4e3ffd710c24ae80e6a8503
seungwoonsong <andrew960811@naver.com>
2023-08-02 08:54:06 +0000

    UPDATE: user testcase added

--- test/user_test.py
+++ test/user_test.py
@@ -19,13 +19,13 @@ def test_read_users():
     assert response.status_code == 200
     # 응답 내용이 정확한지 확인하는 추가 로직이 필요할 수 있습니다.
 
-# def test_make_user():
-#     response = client.post("/users/", headers=headers, json={"name": "test2", "email": "test2@naver.com"})
-#     assert response.status_code == 400
-#     response = client.post("/users/", json={"name": "test2", "email": "asd@naver.com"})
-#     assert response.status_code == 401
+def test_make_user():
+    response = client.post("/users/", json={"name": "test2", "email": "asd@naver.com"})
+    assert response.status_code == 401
+
 def test_get_user_list():
     response = client.get("/users/", headers=headers)
     assert response.status_code == 200
     assert response.json()[0]["name"] == "susong"
     assert response.json()[1]["name"] == "test"
+    assert response.json()[2]["name"] == "test2"
